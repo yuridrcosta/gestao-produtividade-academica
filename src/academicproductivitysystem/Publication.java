9ecb1bc66713d5a45e0f98b72be91d8109baf193
yuridrcosta <38661715+yuridrcosta@users.noreply.github.com>
2018-11-26 23:23:37 -0300

    Update Publication.java

--- src/academicproductivitysystem/Publication.java
+++ src/academicproductivitysystem/Publication.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package academicproductivitysystem;
 
-/**
- *
- * @author Home
- */
+import java.util.ArrayList;
+
 public class Publication {
+    String title;
+    String conference;
+    Project associatedProject = null;
+    int year;
+    public ArrayList<Student> stdAuthors = new ArrayList<Student>();
+    public ArrayList<Professor> profAuthors = new ArrayList<Professor>();
+    public ArrayList<Researcher> resAuthors = new ArrayList<Researcher>();
+
+    public Publication() {
+    }
+
+    public String getConference() {
+        return conference;
+    }
+
+    public void setConference(String conference) {
+        this.conference = conference;
+    }
+
+    public Project getAssociatedProject() {
+        return associatedProject;
+    }
+
+    public void setAssociatedProject(Project associatedProject) {
+        this.associatedProject = associatedProject;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
     
 }
