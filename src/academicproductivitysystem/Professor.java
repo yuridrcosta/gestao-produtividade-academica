f0996b4003a849a0639bb64f667d29fc12efe635
yuridrcosta <38661715+yuridrcosta@users.noreply.github.com>
2019-01-15 14:11:03 -0300

    Hierarchy

--- src/academicproductivitysystem/Professor.java
+++ src/academicproductivitysystem/Professor.java
@@ -4,34 +4,15 @@ package academicproductivitysystem;
 import java.util.ArrayList;
 
 
-public class Professor {
+public class Professor extends Person{
     
-    String name;
-    String email;
     public ArrayList<Publication> publications = new ArrayList<Publication>();
     public ArrayList<Student> orientations = new ArrayList<Student>();
-    public ArrayList<Project> projects = new ArrayList<Project>();
 
     public Professor(String name, String email) {
-        this.name = name;
-        this.email = email;
+        super(name,email);
     }
-
-    public Professor() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public ArrayList<Project> getProjects() {
-        return projects;
-    }
-
+    
     public ArrayList<Student> getOrientations() {
         return orientations;
     }
@@ -40,13 +21,6 @@ public class Professor {
         return publications;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
     
     public void addPublication(Publication p){
         this.publications.add(p);
@@ -54,10 +28,5 @@ public class Professor {
     
     public void addOrientation(Student st){
         this.orientations.add(st);
-    }
-    
-    public void addProject(Project proj){
-        this.projects.add(proj);
-    }
-    
+    }    
 }
