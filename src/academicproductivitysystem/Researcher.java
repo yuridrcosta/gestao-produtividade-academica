ac682d51704dc070c00b560886c26de7ff0720eb
yuridrcosta <38661715+yuridrcosta@users.noreply.github.com>
2019-01-15 14:11:52 -0300

    Hierarchy

--- src/academicproductivitysystem/Researcher.java
+++ src/academicproductivitysystem/Researcher.java
@@ -3,51 +3,20 @@ package academicproductivitysystem;
 
 import java.util.ArrayList;
 
-public class Researcher {
+public class Researcher extends Person{
     
-    String name;
-    String email;
     public ArrayList<Publication> publications = new ArrayList<Publication>();
-    public ArrayList<Project> projects = new ArrayList<Project>();
 
     public Researcher(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public Researcher() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public ArrayList<Project> getProjects() {
-        return projects;
+        super(name,email);
     }
 
     public ArrayList<Publication> getPublications() {
         return publications;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
     
     public void addPublication(Publication p){
         this.publications.add(p);
-    }
-    
-    public void addProject(Project proj){
-        this.projects.add(proj);
-    }
+    } 
     
 }
